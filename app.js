e31449c3a3621a8892f2d74540544dea8a3d193c
ati44 <altunum@hotmail.com>
2024-09-27 16:14:35 +0300

    add view engine

--- app.js
+++ app.js
@@ -3,10 +3,22 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+//ejs template engine
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public")); // Static files
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("index");
 });
 
+app.get("/about.html", (req, res) => {
+  res.send("about");
+});
+app.get("/contact", (req, res) => {
+  res.send("Contact Us");
+});
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
